77a466c4f43a9cb5229a073beac81098295b8c17
Asmwow <aungsanm.dev@gmail.com>
2024-12-06 16:41:54 +0700

    1st

---
+++ OldPhonePad/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OldPhonePad
+{
+    public class OldPhoneKeyPad
+    {
+        public static string OldPhonePad(string input) {
+
+            return "1asdf";
+        }
+    }
+}
