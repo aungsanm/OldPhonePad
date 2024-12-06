d4082140f35368727d8d3ec4a7579996a7479bf8
Asmwow <aungsanm.dev@gmail.com>
2024-12-06 17:04:30 +0700

    string input changed

--- OldPhonePad/Program.cs
+++ OldPhonePad/Program.cs
@@ -7,7 +7,7 @@ public class Program
         Console.WriteLine(OldKeypadPhone.OldPadPhone("33#"));
         Console.WriteLine(OldKeypadPhone.OldPadPhone("227*#"));
         Console.WriteLine(OldKeypadPhone.OldPadPhone("4433555 555666#"));
-        Console.WriteLine(OldKeypadPhone.OldPadPhone("8 88777444666* 664#"));
+        Console.WriteLine(OldKeypadPhone.OldPadPhone("8 88777444666*664#"));
 
         string input = Console.ReadLine();
 
